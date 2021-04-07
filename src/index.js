0b3f8b551d246dc5a3ac1471001d66c8c2011bf3
Ajithshanth <ajithshanth.m@gmail.com>
2021-04-07 13:58:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom';
 import './index.css';
-//import App from "./App"
-
-
-//import { Provider } from 'react-redux';
-//import { createStore } from 'redux';
-//import Counter from './Counter';
 
 
 
